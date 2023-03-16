de24381d3db788e64b18424db4ddff018d00c513
SKY-LEO <leonid311203@gmail.com>
2023-03-16 23:50:21 +0300

    upgrade

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import string
-
 import PySimpleGUI as sg
 import requests
 from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
+from wordcloud import WordCloud
+from collections import Counter
+import matplotlib.pyplot as plt
+import pandas as pd
+
 
 internal_urls = set()
 external_urls = set()
@@ -61,51 +64,10 @@ def task1():
     window.close()
 
 
-def is_valid(url):
-    """
-    Проверяет, является ли 'url' действительным URL
-    """
-    parsed = urlparse(url)
-    return bool(parsed.netloc) and bool(parsed.scheme)
-
-
-def crawl(url, headers):
-    """
-    Сканирует веб-страницу и извлекает все ссылки.
-    Вы найдете все ссылки в глобальных переменных набора external_urls и internal_urls.
-    """
-    print(f"[*] Проверена ссылка: {url}")
-    domain_name = urlparse(url).netloc
-    soup = BeautifulSoup(requests.get(url, headers=headers).content, "html.parser")
-    for a_tag in soup.findAll("a"):
-        href = a_tag.attrs.get("href")
-        if href == "" or href is None:
-            # href пустой тег
-            continue
-        # присоединяемся к URL, если он относительный (не абсолютная ссылка)
-        href = urljoin(url, href)
-        parsed_href = urlparse(href)
-        # удалить GET-параметры URL, фрагменты URL и т. д.
-        href = parsed_href.scheme + "://" + parsed_href.netloc + parsed_href.path
-        if not is_valid(href):
-            # недействительный URL
-            continue
-        if href in internal_urls:
-            # уже в наборе
-            continue
-        if domain_name not in href:
-            # внешняя ссылка
-            if href not in external_urls:
-                print(f"[!] Внешняя ссылка: {href}")
-                external_urls.add(href)
-            continue
-        print(f"[*] Внутренняя ссылка: {href}")
-        internal_urls.add(href)
-
-
 def task2():
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/61.0.3163.100 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Language': 'ru-ru,ru;q=0.8,en-us;q=0.5,en;q=0.3',
         'Accept-Encoding': 'gzip, deflate',
@@ -115,11 +77,11 @@ def task2():
     url = "https://vk.com/sky_leo"
     url = "https://onliner.by"
     url = "https://bsuir.by"
-    url = "https://belta.by"
+    #url = "https://www.microsoft.com/en-us"
     crawl(url, headers)
-    print("[+] Итого внутренних ссылок:", len(internal_urls))
-    print("[+] Итого внешних ссылок:", len(external_urls))
-    print("[+] Итого URL:", len(external_urls) + len(internal_urls))
+    print("Итого внутренних ссылок:", len(internal_urls))
+    print("Итого внешних ссылок:", len(external_urls))
+    print("Итого URL:", len(external_urls) + len(internal_urls))
     domain_name = urlparse(url).netloc
     # сохраняем внутренние ссылки в файл
     with open(f"{domain_name}_internal_links.txt", "w") as f:
@@ -132,16 +94,100 @@ def task2():
 
     soup = BeautifulSoup(requests.get(url, headers=headers).text, 'lxml')
     text = soup.getText().lower()
-    gg = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~1234567890«–»"""
-    for character in gg:
-        text = text.replace(character, ' ')
+    symbols_to_replace = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~1234567890«–»"""
+    special_symbols = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~«–»"""
+    english_alphabet = "qwertyuiopasdfghjklzxcvbnm"
+    russian_alphabet = "ёйцукенгшщзхъфывапролджэячсмитьбю"
 
-    # text1 = text.translate(str.maketrans('', '', string.punctuation))
-    text = text.split()
-    # text = text.replace(string.punctuation, '')
-    print(text)
-    print(len(text))
-    # print(text1)
+    num_of_special_symbols = count_symbols(special_symbols, text)
+    num_of_english_symbols = count_symbols(english_alphabet, text)
+    num_of_russian_symbols = count_symbols(russian_alphabet, text)
+    print("Количество спец. символов:", num_of_special_symbols)
+    print("Количество русских букв:", num_of_russian_symbols)
+    print("Количество английских букв:", num_of_english_symbols)
+
+    original_text = text
+    for character in symbols_to_replace:
+        text = text.replace(character, '')
+
+    splitted_text = text.split()
+    print(splitted_text)
+    print("Количество букв:", len(text))
+    print("Количество слов:", len(splitted_text))
+    wordcloud = WordCloud().generate(text)
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
+    english_freq = frequency(english_alphabet, text)
+    print(english_freq)
+    plotting_diagram(english_freq, english_alphabet)
+    russian_freq = frequency(russian_alphabet, text)
+    print(russian_freq)
+    plotting_diagram(russian_freq, russian_alphabet)
+    special_sym_freq = frequency(special_symbols, original_text)
+    print(special_sym_freq)
+    plotting_diagram(special_sym_freq, special_symbols)
+
+
+def count_symbols(alphabet, text):
+    counter = 0
+    for symbol in alphabet:
+        for character in text:
+            if character == symbol:
+                counter += 1
+    return counter
+
+
+def count_symbol(symbol, text):
+    counter = 0
+    for character in text:
+        if character == symbol:
+            counter += 1
+    return counter
+
+
+def frequency(symbols, text):
+    arr_frequency = []
+    for symbol in symbols:
+        arr_frequency.append(count_symbol(symbol, text))
+    return arr_frequency
+
+
+def plotting_diagram(arr_my_frequency, symbols):
+    fig, ax = plt.subplots()
+    xs = range(len(symbols))
+    ax.bar(xs, arr_my_frequency, color="blue")
+    plt.xticks(xs, symbols)
+    plt.show()
+
+
+def is_valid(url):
+    parsed = urlparse(url)
+    return bool(parsed.netloc) and bool(parsed.scheme)
+
+
+def crawl(url, headers):
+    print("Проверена ссылка:", url)
+    domain_name = urlparse(url).netloc
+    soup = BeautifulSoup(requests.get(url, headers=headers).content, "html.parser")
+    for a_tag in soup.findAll("a"):
+        href = a_tag.attrs.get("href")
+        if href == "" or href is None:  # если href пустой тег
+            continue
+        href = urljoin(url, href)  # присоединяемся к URL, если он относительный (не абсолютная ссылка)
+        parsed_href = urlparse(href)
+        href = parsed_href.scheme + "://" + parsed_href.netloc + parsed_href.path  # удаляем все лишнее в URL
+        if not is_valid(href):  # если недействительный URL
+            continue
+        if href in internal_urls:  # если уже встречался
+            continue
+        if domain_name not in href:  # если доменное имя не совпадает, то - внешняя ссылка
+            if href not in external_urls:  # если ещё не встречался
+                print("Внешняя ссылка:", href)
+                external_urls.add(href)
+            continue
+        print("Внутренняя ссылка:", href)
+        internal_urls.add(href)
 
 
 def menu():
